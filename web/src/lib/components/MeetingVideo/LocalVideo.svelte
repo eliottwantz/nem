1835986ffe20e40a381e8ed873da510a531ed1f8
Eliott Wantz <93885811+DevEliott@users.noreply.github.com>
2023-10-18 17:31:08 +0200

    prebuilt ui video conference with patch

--- web/src/lib/components/MeetingVideo/LocalVideo.svelte
+++ web/src/lib/components/MeetingVideo/LocalVideo.svelte
@@ -1,46 +1,132 @@
 <script lang="ts">
 	import ParticipantIcon from '$lib/icons/ParticipantIcon.svelte'
 	import { onMount } from 'svelte'
-	import type { MeetingVideoTracks } from '.'
+	import CameraIcon from '$lib/icons/CameraIcon.svelte'
+	import CameraOffIcon from '$lib/icons/CameraOffIcon.svelte'
+	import MicrophoneIcon from '$lib/icons/MicrophoneIcon.svelte'
+	import MicrophoneMutedIcon from '$lib/icons/MicrophoneMutedIcon.svelte'
 
-	export let tracks: MeetingVideoTracks
-	export let videoEnabled: boolean | undefined = undefined
+	let audioEnabled = false
+	let videoEnabled = false
+	let videoDevice: MediaDeviceInfo | undefined = undefined
+	let audioDevice: MediaDeviceInfo | undefined = undefined
+
+	let audioDevices: MediaDeviceInfo[] = []
+	let videoDevices: MediaDeviceInfo[] = []
 
 	let videoElm: HTMLVideoElement
 	let audioElem: HTMLAudioElement
 
-	onMount(() => {
-		tracks.videoTrack?.attach(videoElm)
-		console.log('mount local meeting video component. tracks:', tracks)
-
-		return () => {
-			console.log('destroy local meeting video component. tracks:', tracks)
-			tracks.videoTrack?.detach()
+	onMount(async () => {
+		if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
+		} else {
+			console.log('Camera and microphone not supported in this browser')
+			alert('Camera and microphone not supported in this browser')
+			return
 		}
+		const devices = await navigator.mediaDevices.enumerateDevices()
+		videoDevices = devices.filter((d) => d.kind === 'videoinput')
+		videoDevice = videoDevices[0]
+		audioDevices = devices.filter((d) => d.kind === 'audioinput')
+		audioDevice = audioDevices[0]
 	})
 
-	$: if (tracks.videoTrack && videoElm) {
-		console.log('attaching video track')
-		tracks.videoTrack.attach(videoElm)
+	async function startStream(constraints: MediaStreamConstraints) {
+		const stream = await navigator.mediaDevices.getUserMedia(constraints)
+		videoElm.srcObject = stream
 	}
 
-	$: console.log(tracks)
+	async function toggleAudio() {
+		audioEnabled = !audioEnabled
+	}
+
+	async function toggleVideo() {
+		let text = videoEnabled ? 'closing camera' : 'opening camera'
+		console.log(text)
+
+		if (!videoEnabled) {
+			await startStream({
+				video: {
+					deviceId: {
+						exact: videoDevice?.deviceId
+					}
+				}
+			})
+		} else {
+			const stream = videoElm.srcObject
+			if (stream) {
+				const tracks = (stream as MediaStream).getTracks()
+				tracks.forEach((t) => t.stop())
+			}
+			videoElm.srcObject = null
+		}
+
+		videoEnabled = !videoEnabled
+	}
 </script>
 
-<div class="relative aspect-video overflow-hidden rounded-lg bg-black">
-	<audio bind:this={audioElem} />
-	<!-- svelte-ignore a11y-media-has-caption -->
-	<video
-		bind:this={videoElm}
-		autoplay
-		unselectable="on"
-		playsinline
-		disablepictureinpicture
-		class="pointer-events-none aspect-video w-full object-cover"
-	/>
-	{#if videoEnabled === false}
-		<div class="absolute left-0 top-0 grid aspect-video w-full place-items-center">
-			<ParticipantIcon class="h-[70%] max-w-[100%]" />
+<div id="prejoin" class="w-[min(100%,30rem)] space-y-2 p-2">
+	<div class="relative aspect-video overflow-hidden rounded-lg bg-black">
+		<audio bind:this={audioElem} />
+		<!-- svelte-ignore a11y-media-has-caption -->
+		<video
+			bind:this={videoElm}
+			autoplay
+			unselectable="on"
+			playsinline
+			disablepictureinpicture
+			class="pointer-events-none aspect-video w-full object-cover"
+		/>
+		{#if videoEnabled === false}
+			<div class="absolute left-0 top-0 grid aspect-video w-full place-items-center">
+				<ParticipantIcon class="h-[70%] max-w-[100%]" />
+			</div>
+		{/if}
+	</div>
+	<div id="devices" class="flex gap-4">
+		<div class="flex w-1/2">
+			<button
+				on:click={toggleVideo}
+				class="variant-filled-surface btn flex select-none gap-1 p-2"
+				title="Toggle camera"
+			>
+				{#if videoEnabled}
+					<CameraIcon class="h-6 w-6" />
+				{:else}
+					<CameraOffIcon class="h-6 w-6" />
+				{/if}
+				Camera
+			</button>
+			<select
+				on:change={() => startStream({ video: { deviceId: videoDevice?.deviceId } })}
+				class="select"
+				name="video"
+				bind:value={videoDevice}
+			>
+				{#each videoDevices as device}
+					<option value={device}>{device.label}</option>
+				{/each}
+			</select>
 		</div>
-	{/if}
+		<div class="flex w-1/2">
+			<button
+				on:click={toggleAudio}
+				class="variant-filled-surface btn flex select-none gap-1 p-2"
+				title="Toggle microphone"
+			>
+				{#if audioEnabled}
+					<MicrophoneIcon class="h-6 w-6" />
+				{:else}
+					<MicrophoneMutedIcon class="h-6 w-6" />
+				{/if}
+				Microphone
+			</button>
+			<select class="select" name="audio" bind:value={audioDevice}>
+				{#each audioDevices as device}
+					<option value={device}>{device.label}</option>
+				{/each}
+			</select>
+		</div>
+	</div>
+	<slot />
 </div>
--- web/src/routes/videosdk/+page.svelte
+++ web/src/routes/videosdk/+page.svelte
@@ -1,170 +1,61 @@
 <script lang="ts">
-	import { PUBLIC_VIDEO_SDK_TEMP_TOKEN } from '$env/static/public'
-	import { Meeting, Participant, VideoSDK } from '@videosdk.live/js-sdk'
+	import { PUBLIC_VIDEO_SDK_API_KEY } from '$env/static/public'
+	import { dev } from '$app/environment'
+	import LocalVideo from '$lib/components/MeetingVideo/LocalVideo.svelte'
+	//@ts-expect-error
+	import VideoSDKMeeting from '@videosdk.live/rtc-js-prebuilt/dist/index'
 
-	let videoContainer: HTMLDivElement
-	let textDiv: HTMLDivElement
-	let participants: Participant[] = []
-	let meeting: Meeting | null
-	let localParticipantVideo: HTMLVideoElement | undefined
-	let remoteParticipantId = ''
+	let hasJoined = false
 
-	function handleClick(joinButton: HTMLButtonElement) {
-		const evv = function () {
-			joinButton.style.display = 'none'
-			textDiv.textContent = 'Please wait, we are joining the meeting'
+	function initMeeting() {
+		hasJoined = true
+		const config = {
+			debug: dev ? true : false,
+			apiKey: PUBLIC_VIDEO_SDK_API_KEY,
+			meetingId: 'milkyway',
+			name: 'Demo User',
+			containerId: 'meetingDiv',
 
-			VideoSDK.config(PUBLIC_VIDEO_SDK_TEMP_TOKEN)
-			meeting = VideoSDK.initMeeting({
-				meetingId: 'ue0p-stx9-l8y3', // required
-				name: "Eliott's Org", // required
-				micEnabled: false, // optional, default: true
-				webcamEnabled: false, // optional, default: true
-				chatEnabled: false,
-				maxResolution: 'sd'
-			})
+			micEnabled: false,
+			webcamEnabled: false,
+			participantCanToggleSelfWebcam: true,
+			participantCanToggleSelfMic: true,
 
-			meeting.join()
+			chatEnabled: true,
+			screenShareEnabled: true,
+			screenShareConfig: {
+				resolution: 'h720p_15fps',
+				optimizationMode: 'text'
+			},
+			canCreatePoll: true,
+			whiteboardEnabled: true,
 
-			meeting.on('meeting-joined', () => {
-				textDiv.style.display = 'none'
-			})
-
-			// other participants
-			meeting.on('participant-joined', (participant: Participant) => {
-				participants = [...participants, participant]
-				remoteParticipantId = participant.id
-			})
-
-			// participants left
-			meeting.on('participant-left', (participant) => {
-				participants = participants.filter((p) => p.id !== participant.id)
-			})
-		}
-
-		joinButton.addEventListener('click', evv)
-
-		return {
-			destroy() {
-				joinButton.removeEventListener('click', evv)
+			theme: 'LIGHT',
+			branding: {
+				enabled: true,
+				logoURL:
+					'https://prfhxmmcrvholuplxcad.supabase.co/storage/v1/object/public/assets/logo_400x400.png?t=2023-10-18T13%3A36%3A05.812Z',
+				name: 'NEM',
+				poweredBy: false
 			}
 		}
-	}
 
-	function handleStreamTrack(
-		divContainer: HTMLDivElement,
-		{ participant }: { participant: Participant }
-	) {
-		let audioElement = divContainer.querySelector('audio')!
-		participant.on('stream-enabled', (stream) => {
-			setTrack(stream, audioElement, participant, false)
-		})
-
-		return {
-			destroy() {},
-			update({ participant }: { participant: Participant }) {
-				console.log('participant update action', participant)
-			}
-		}
-	}
-
-	// setting media track
-	function setTrack(
-		stream: any,
-		audioElement: HTMLAudioElement,
-		participant: Participant,
-		isLocal: boolean
-	) {
-		if (stream.kind == 'video') {
-			const mediaStream = new MediaStream()
-			mediaStream.addTrack(stream.track)
-			let videoElm = document.getElementById(`v-${participant.id}`)! as HTMLVideoElement
-			videoElm.srcObject = mediaStream
-			videoElm
-				.play()
-				.catch((error) => console.error('videoElem.current.play() failed', error))
-		}
-		if (stream.kind == 'audio' && !isLocal) {
-			const mediaStream = new MediaStream()
-			mediaStream.addTrack(stream.track)
-			audioElement.srcObject = mediaStream
-			audioElement.play().catch((error) => console.error('audioElem.play() failed', error))
-		}
+		const meeting = new VideoSDKMeeting()
+		meeting.init(config)
 	}
 </script>
 
-<svelte:head>
-	<script>
-		var script = document.createElement('script')
-		script.type = 'text/javascript'
-
-		script.addEventListener('load', function (event) {
-			const config = {
-				name: 'Demo User',
-				meetingId: 'milkyway',
-				apiKey: 'df7979b9-fb37-41a7-b480-9ade89552f70',
-
-				containerId: 'meetingDiv',
-
-				micEnabled: true,
-				webcamEnabled: true,
-				participantCanToggleSelfWebcam: true,
-				participantCanToggleSelfMic: true,
-
-				chatEnabled: true,
-				screenShareEnabled: true,
-				canCreatePoll: true,
-				whiteboardEnabled: true,
-
-				theme: 'LIGHT',
-				branding: {
-					enabled: true,
-					logoURL:
-						'https://prfhxmmcrvholuplxcad.supabase.co/storage/v1/object/public/assets/logo_400x400.png?t=2023-10-18T13%3A36%3A05.812Z',
-					name: 'NEM',
-					poweredBy: false
-				}
-			}
-
-			const meeting = new VideoSDKMeeting()
-			meeting.init(config)
-		})
-
-		script.src = 'https://sdk.videosdk.live/rtc-js-prebuilt/0.3.33/rtc-js-prebuilt.js'
-		document.getElementsByTagName('head')[0].appendChild(script)
-	</script>
-</svelte:head>
-
 <h1 class="h1 text-center">Video SDK</h1>
-
-<button disabled use:handleClick>Join meeting</button>
-
-<div bind:this={textDiv} id="textDiv"></div>
-
-<div class="h-full w-full" id="meetingDiv"></div>
-{#if meeting}
-	<div class="row" bind:this={videoContainer} id="videoContainer">
-		<!-- Local participant -->
-		{#if meeting.localParticipant}
-			<!-- svelte-ignore a11y-media-has-caption -->
-			<video
-				bind:this={localParticipantVideo}
-				class="video-frame"
-				playsinline
-				width="300"
-				id="v-{meeting.localParticipant.id}"
-			>
-			</video>
-		{/if}
-		<!-- Local participant -->
-
-		{#each participants as participant}
-			<div use:handleStreamTrack={{ participant }}>
-				<!-- svelte-ignore a11y-media-has-caption -->
-				<video class="video-frame" playsinline width="300" id="v-{participant.id}"> </video>
-				<audio autoplay={false} playsinline controls={false} id="a-{participant.id}">
-				</audio>
-			</div>
-		{/each}
+{#if hasJoined}
+	<div class="h-full w-full" id="meetingDiv"></div>
+{:else}
+	<div class="grid h-full place-items-center">
+		<div class="flex w-max flex-col items-center justify-center">
+			<LocalVideo>
+				<button class="variant-filled-primary btn w-full" on:click={initMeeting}>
+					Join class
+				</button>
+			</LocalVideo>
+		</div>
 	</div>
 {/if}
